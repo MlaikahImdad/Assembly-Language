5df913ac44726ada7521cef5f782bbaf7bd823ef
Mlaikah Imdad <120927833+MlaikahImdad@users.noreply.github.com>
2024-06-16 11:37:39 +0500

    Add files via upload

---
+++ Disable_keyboard_(Interrupt_in_PIC_maskReg).asm
@@ -0,0 +1,9 @@
+; Disable keyboard interrupt in PIC mask register
+[org 0x0100]
+
+mov al, 0x21        ; read interrupt mask register
+or al, 2            ; set bit for IRQ2
+out 0x21, al        ; write back mask register
+
+mov ax, 0x4c00      ; terminate program
+int 0x21
